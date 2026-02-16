193174d411fc0a7e6f14f48b3d29d66eba113474
Ed0827 <seyun.jeong@slu.edu>
2026-02-16 11:52:01 -0600

    added the map behind of the card

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
 export const dynamic = "force-dynamic"; // This disables SSG and ISR
 
-import prisma from "@/lib/prisma";
 import ProjectInfoCard from "./info-card/page";
+import MapWrapper from "./map/MapWrapper";
 
 export default async function Home() {
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-row items-left py-24 px-8">
-      <ProjectInfoCard location={"63103"} fedFunds={1000000} avgPropertyValue={500000}/>
+    <div className="relative min-h-screen">
+      {/* Map - Full screen background */}
+      <div className="absolute inset-0 z-0">
+        <MapWrapper />
+      </div>
+      {/* Content floating on top of the map */}
+      <div className="relative z-10 min-h-screen flex flex-row items-left py-24 px-8">
+        <ProjectInfoCard location={"63103"} fedFunds={1000000} avgPropertyValue={500000}/>
+      </div>
     </div>
   );
 }
