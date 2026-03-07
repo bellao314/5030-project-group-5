a8f3deae055e58b5991aef3c1094a8248ac7dfdd
Bella Ott <bella.c.ott.04@gmail.com>
2026-03-07 13:08:33 -0600

    moved info-card and map into components folder

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,6 +1,6 @@
 // App.jsx
 import React, { useState } from 'react';
-import InteractiveMap from './map/map';
+import InteractiveMap from './components/map/map';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './App.css';
